349c07949d2d91556e76cfd384e3f29ba7a63f17
bjpatterson <bjpatterson@bsu.edu>
2017-02-22 02:11:56 -0500

    Separate root-segregated files into folders
    for less clutter

--- Scripts/SplitSpidersByRoot.py
+++ Scripts/SplitSpidersByRoot.py
@@ -2,8 +2,11 @@
 Read the outputs from `GracefulSpiderEnumerator`, and segregate the spiders by root label
 """
 
+import os
+
 SPIDER_FILE = 'results/spider_enum_order_{:0>3}.txt'
-SEPARATED_FILE = 'results/order_{:0>3}_root_{:0>3}.txt'
+SEPARATED_FOLDER = 'results/order_{0}'
+SEPARATED_FILE = 'order_{0:0>3}_root_{1:0>3}.txt'
 
 def detect_root(edge_list):
     for i in range(len(edge_list) + 1):
@@ -16,9 +19,12 @@ def separate_spiders(order):
     for line in open(SPIDER_FILE.format(order), 'r').readlines():
         edges = eval("list({})".format(line))
         root = detect_root(edges)
-        with open(SEPARATED_FILE.format(order, root), 'a') as outfile:
+        if not os.path.exists(SEPARATED_FOLDER.format(order)):
+            os.makedirs(SEPARATED_FOLDER.format(order))
+        out_path = SEPARATED_FOLDER + "/" + SEPARATED_FILE
+        with open(out_path.format(order, root), 'a') as outfile:
             outfile.write(line)
 
 if __name__ == '__main__':
-    for i in range(4, 15):
+    for i in range(4, 6):
         separate_spiders(i)
