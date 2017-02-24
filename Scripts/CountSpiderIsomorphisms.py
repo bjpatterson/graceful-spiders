26d725e22c963e346fec0db4e0384a2e1dc39374
bjpatterson <bjpatterson@bsu.edu>
2017-02-24 17:13:32 -0500

    count isomorphism with a given leaf node
    for science

--- Scripts/CountSpiderIsomorphisms.py
+++ Scripts/CountSpiderIsomorphisms.py
@@ -17,6 +17,9 @@ ALPHA_ROOTED_SPIDER_FILE = 'order_{0:0>3}_root_{1:0>3}.txt'
 ALPHA_ROOTED_SPIDER_FOLDER = 'results/alpha_order_{0}'
 ALPHA_ROOTED_SUMMARY_FILE = 'iso_count_order_{0:0>3}_root_{1:0>3}.txt'
 
+LEAF_SUMMARY_FILE = 'iso_leaf_count_order_{0:0>3}.txt'
+ALPHA_LEAF_SUMMARY_FILE = 'alpha_leaf_count_order_{0:0>3}.txt'
+
 
 def detect_root(edge_list):
     """given an edge set for a spider, return the root node's label"""
@@ -78,6 +81,57 @@ def count_isomorphisms(order):
             outfile.write("{}: {}\n".format(iso[1], iso[0]))
 
 
+def spider_has_leaf(edges, leaf_id):
+    return len([edge for edge in edges if leaf_id in edge]) == 1
+
+
+def count_leaf_isomorphisms(order):
+    counts = dict()
+    for line in open(BASIC_SPIDER_FILE.format(order), 'r').readlines():
+        edges = eval("list({})".format(line))
+        isomorphism = detect_isomorphism(edges)
+        for label in range(order):
+            if spider_has_leaf(edges, label):
+                if (repr(isomorphism), label) in counts:
+                    counts[(repr(isomorphism), label)] += 1
+                else:
+                    counts.update({(repr(isomorphism), label): 1})
+
+    all_isos = [(key, val) for key, val in counts.iteritems()]
+    all_isos.sort(key=operator.itemgetter(1), reverse=True)
+
+    outfile = BASIC_SUMMARY_FOLDER + "/" + LEAF_SUMMARY_FILE
+    with open(outfile.format(order), 'w') as outfile:
+        outfile.write('isomorphism;leaf_label;count\n')
+        for iso in all_isos:
+            outfile.write("{};{};{}\n".format(iso[0][0], iso[0][1], iso[1]))
+
+
+def count_leaf_alpha_isos(order):
+    counts = dict()
+    file_path = ALPHA_ROOTED_SPIDER_FOLDER + "\\" + ALPHA_ROOTED_SPIDER_FILE
+    for root in range(order):
+        if os.path.exists(file_path.format(order, root)):
+            for line in open(file_path.format(order, root), 'r').readlines():
+                edges = eval("list({})".format(line))
+                isomorphism = detect_isomorphism(edges, root)
+                for label in range(order):
+                    if spider_has_leaf(edges, label):
+                        if (repr(isomorphism), label) in counts:
+                            counts[(repr(isomorphism), label)] += 1
+                        else:
+                            counts.update({(repr(isomorphism), label): 1})
+
+    all_isos = [(key, val) for key, val in counts.iteritems()]
+    all_isos.sort(key=operator.itemgetter(1), reverse=True)
+
+    outfile_path = BASIC_SUMMARY_FOLDER + "/" + ALPHA_LEAF_SUMMARY_FILE
+    with open(outfile_path.format(order), 'w') as outfile:
+        outfile.write('isomorphism;leaf_label;count\n')
+        for iso in all_isos:
+            outfile.write("{};{};{}\n".format(iso[0][0], iso[0][1], iso[1]))
+
+
 def count_rooted_isomorphisms(order, root):
     counts = dict()
     file_path = ROOTED_SPIDER_FOLDER + "\\" + ROOTED_SPIDER_FILE
@@ -123,7 +177,13 @@ def count_alpha_rooted_isomorphisms(order, root):
 if __name__ == '__main__':
     for order in range(4, 17):
         # count_isomorphisms(order)
+
         # for root in range(order):
         #     count_rooted_isomorphisms(order, root)
-        for root in range(order):
-            count_alpha_rooted_isomorphisms(order, root)
+
+        # for root in range(order):
+        #     count_alpha_rooted_isomorphisms(order, root)
+
+        # count_leaf_isomorphisms(order)
+
+        count_leaf_alpha_isos(order)
